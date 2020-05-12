972d82301a748922c647713c94f84a3b4d2c5073
znjuko <48394965+znjuko@users.noreply.github.com>
2020-05-12 19:07:45 +0300

    Update main.go

--- main.go
+++ main.go
@@ -73,6 +73,10 @@ func Logs(next echo.HandlerFunc) echo.HandlerFunc {
 			err = next(rwContext)
 			respTime := time.Since(start)
 			fmt.Println("MICRO SEC:", respTime.Microseconds(), "\n PATH:", rwContext.Request().URL.Path, "\n METHOD:", rwContext.Request().Method)
+			if respTime.Milliseconds() >= 400 {
+				fmt.Println(rwContext.QueryParam("sort"))
+			}
+		
 		} else {
 			err = next(rwContext)
 		}

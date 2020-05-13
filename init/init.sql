bc83b518302c9494899eb191b2e9a10e496e9e74
znjuko <48394965+znjuko@users.noreply.github.com>
2020-05-13 16:33:36 +0300

    Update init.sql

--- init/init.sql
+++ init/init.sql
@@ -9,7 +9,7 @@ CREATE EXTENSION IF NOT EXISTS CITEXT;
 DROP TRIGGER IF EXISTS path_updater ON messages;
 DROP FUNCTION IF EXISTS updater;
 
-CREATE UNLOGGED TABLE users
+CREATE TABLE users
 (
     u_id     BIGSERIAL PRIMARY KEY,
     nickname CITEXT COLLATE "C" UNIQUE,
@@ -18,10 +18,9 @@ CREATE UNLOGGED TABLE users
     about    TEXT
 );
 
-CREATE INDEX idx_users_nickname ON users(nickname,fullname,email,about);
-CLUSTER users USING idx_users_nickname;
+CREATE INDEX idx_users_nickname ON users(nickname);
 
-CREATE UNLOGGED TABLE forums
+CREATE TABLE forums
 (
     f_id            BIGSERIAL PRIMARY KEY,
     slug            CITEXT UNIQUE NOT NULL,
@@ -33,10 +32,9 @@ CREATE UNLOGGED TABLE forums
 
 
 CREATE INDEX idx_forums_slug ON forums(slug);
-CLUSTER forums USING idx_forums_slug;
 
 
-CREATE UNLOGGED TABLE threads
+CREATE TABLE threads
 (
     t_id       BIGSERIAL PRIMARY KEY,
     slug       CITEXT UNIQUE,
@@ -49,12 +47,11 @@ CREATE UNLOGGED TABLE threads
 );
 
 
-CREATE INDEX idx_threads_fslugdate ON threads(f_slug,date,t_id,slug,message,title,votes,u_nickname);
+CREATE INDEX idx_threads_fslugdate ON threads (f_slug,date);
 CLUSTER threads USING idx_threads_fslugdate;
-CREATE INDEX idx_threads_slugtid ON threads(slug,t_id);
-CREATE INDEX idx_threads_tidslug ON threads(t_id,slug);
+CREATE INDEX idx_threads_slughash ON threads (slug);
 
-CREATE UNLOGGED TABLE voteThreads
+CREATE TABLE voteThreads
 (
     vt_id      BIGSERIAL,
     t_id       BIGINT             NOT NULL REFERENCES threads ON DELETE CASCADE,
@@ -62,9 +59,9 @@ CREATE UNLOGGED TABLE voteThreads
     u_nickname CITEXT COLLATE "C" NOT NULL REFERENCES users (nickname) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX idx_voteth_thrnick ON voteThreads(t_id, u_nickname);
+CREATE UNIQUE INDEX idx_voteth_thrnick ON voteThreads USING btree (t_id, u_nickname);
 
-CREATE UNLOGGED TABLE messages
+CREATE TABLE messages
 (
     m_id       BIGSERIAL PRIMARY KEY,
     date       TIMESTAMP WITH TIME ZONE,
@@ -80,10 +77,10 @@ CREATE UNLOGGED TABLE messages
 CREATE INDEX idx_messages_tid_mid ON messages (t_id, m_id);
 CREATE INDEX idx_messages_parent_tree_tid_parent ON messages (parent,t_id,m_id,(path[1]));
 CLUSTER messages USING idx_messages_parent_tree_tid_parent;
-CREATE INDEX idx_messages_path_1 ON messages((path[1]));
+CREATE INDEX idx_messages_path_1 ON messages ((path[1]));
 CREATE INDEX idx_messages_path ON messages (path,m_id);
 
-CREATE UNLOGGED TABLE forumUsers
+CREATE TABLE forumUsers
 (
     f_slug     CITEXT COLLATE "C" NOT NULL REFERENCES forums (slug) ON DELETE CASCADE,
     u_nickname CITEXT COLLATE "C" NOT NULL REFERENCES users (nickname) ON DELETE CASCADE,
@@ -92,7 +89,7 @@ CREATE UNLOGGED TABLE forumUsers
 
 CREATE INDEX idx_forumusers_slug_nick ON forumUsers (f_slug, u_nickname);
 CLUSTER forumUsers USING idx_forumusers_slug_nick;
-
+CREATE INDEX idx_forumusers_nick ON forumUsers (u_nickname);
 
 
 CREATE OR REPLACE FUNCTION updater()
@@ -109,4 +106,3 @@ CREATE TRIGGER path_updater
     ON messages
     FOR EACH ROW
 EXECUTE PROCEDURE updater();
-
